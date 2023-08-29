80d92fd647ba496f4109e85676dc14ce429adf68
PandaFlo <96489534+PandaFlo@users.noreply.github.com>
2023-08-29 15:25:27 -0400

    Add files via upload

--- Leet/C/code/reverseWordInString.c
+++ Leet/C/code/reverseWordInString.c
@@ -1,41 +1,39 @@
 
 /*
-There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+Given an input string s, reverse the order of the words.
 
-Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
 
-Note that multiple kids can have the greatest number of candies.
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
 
  
 
 Example 1:
 
-Input: candies = [2,3,5,1,3], extraCandies = 3
-Output: [true,true,true,false,true] 
-Explanation: If you give all extraCandies to:
-- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
-- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
-- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
-- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
-- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+Input: s = "the sky is blue"
+Output: "blue is sky the"
 Example 2:
 
-Input: candies = [4,2,1,1,2], extraCandies = 1
-Output: [true,false,false,false,false] 
-Explanation: There is only 1 extra candy.
-Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
 Example 3:
 
-Input: candies = [12,1,12], extraCandies = 10
-Output: [true,false,true]
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
  
 
 Constraints:
 
-n == candies.length
-2 <= n <= 100
-1 <= candies[i] <= 100
-1 <= extraCandies <= 50
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ 
+
+Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
 
 */
 #include <stdio.h>
